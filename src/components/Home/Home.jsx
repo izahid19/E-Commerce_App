db1df481711c22ec888a739ab6bdc14e85dde6c5
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-07-15 03:30:47 +0530

    homepage popular section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
-import Category from "./Category/Category"
+import Category from "./Category/Category";
+import Products from "../Products/Products";
 const Home = () => {
     return (
        <div className="home">
@@ -9,6 +10,7 @@ const Home = () => {
             <div className="main-content">
                 <div className="layout">
                    <Category/>
+                   <Products/>
                 </div>
             </div>
        </div>
